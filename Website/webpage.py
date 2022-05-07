954c3cb6c4e84dc755957bcc371669e161717f99
patolucatis <patolucatis@gmail.com>
2022-05-07 19:30:47 +0200

    Inicio de Flask
    Adaptación de la página web para poder funcionar en Flask y comienzo de la aplicación en Flask

---
+++ Website/webpage.py
@@ -0,0 +1,14 @@
+import flask
+from flask_cors import CORS
+
+app = flask.Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
