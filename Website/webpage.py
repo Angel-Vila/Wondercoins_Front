afb2a7cf5014e5007d8cfd2aca6e09224c58c2fd
patolucatis <patolucatis@gmail.com>
2022-05-18 16:42:12 +0200

    Google Maps
    Añadida la API de Google Maps en la pagina principal

--- Website/webpage.py
+++ Website/webpage.py
@@ -1,14 +1,24 @@
 import flask
 from flask_cors import CORS
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
 
 app = flask.Flask(__name__)
 CORS(app)
+app.config['GOOGLEMAPS_KEY'] = open("GOOGLE_API.txt").read()
+GoogleMaps(app)
 
 
 @app.route("/")
 def index():
-    return flask.render_template("index.html")
+    mapa = Map(
+        identifier="mapa",
+        lat=36.53813063050466,
+        lng=-6.202028173211203,
+        region="es"
+    )
+    return flask.render_template("index.html", mapa=mapa)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
