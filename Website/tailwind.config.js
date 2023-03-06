3fc0badf1b24bad0d37f52c11e7c2b7745f68425
patolucatis <patolucatis@gmail.com>
2023-03-06 19:44:59 +0100

    requirements and finished base
    Added requirements.txt to the whole project and finished cleaning the website base

--- Website/tailwind.config.js
+++ Website/tailwind.config.js
@@ -4,13 +4,17 @@ module.exports = {
   theme: {
       colors: {
           "fondo": "#d4dfdf",
-          "cabecera": "#03989e"
+          "cabecera": "#03989e",
+          "blanco": "#FFFFFF",
       },
         extend: {
           fontFamily: {
               "josefin": ["Josefin Sans", "sans-serif"],
               "alice": ["Alice", "serif"]
-          }
+          },
+            backgroundImage: {
+              "wondercoins": "../static/logo-sinletras-sinfondo.jpg"
+            }
         },
   },
   plugins: [require("daisyui")],
